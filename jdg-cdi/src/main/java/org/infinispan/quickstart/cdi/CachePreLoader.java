b563dd90dddbc24920fe87d2bb6bbe8b84dc78d8
rafaeltuelho <rafaelcba@gmail.com>
2015-12-08 16:49:07 -0200

    clone from jdg quickstarts. Changed to use modules and JPA

---
+++ jdg-cdi/src/main/java/org/infinispan/quickstart/cdi/CachePreLoader.java
@@ -0,0 +1,42 @@
+package org.infinispan.quickstart.cdi;
+
+import java.util.logging.Logger;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import org.infinispan.Cache;
+import org.infinispan.quickstart.cdi.config.GreetingCache;
+
+@Startup
+@Singleton
+@ApplicationScoped
+public class CachePreLoader {
+	@Inject
+	private Logger log;
+
+	@Inject
+	@GreetingCache
+	private Cache<String, String> cache;
+	
+	@PostConstruct
+	private void init(){
+		log.info("\n\t Initializing Distributed Cache node\n");
+		
+		for (int i = 0; i < 1000; i++) {
+			cache.put("k" + i, java.util.UUID.randomUUID().toString());			
+		}
+		
+		log.info("\t\t " + cache.size());
+	}
+	
+	@PreDestroy
+	private void cleanUp(){
+		log.info("\n\t Destroying Distributed Cache instance node\n");
+		cache.clear();
+	}
+}
